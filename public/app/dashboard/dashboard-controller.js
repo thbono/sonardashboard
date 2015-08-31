bfbd0775cb0f7e9de9e884fb3e50aa5b6c2be914
rodrigoff <rodrigoff@live.com>
2015-08-31 19:27:27 -0300

    Prototypical inheritance on controllers

--- public/app/dashboard/dashboard-controller.js
+++ public/app/dashboard/dashboard-controller.js
@@ -1,133 +1,161 @@
-﻿dashboardModule
-    .controller('dashboardController', [
-        '$scope', 'issuesService', 'usersService', 'projectsService',
-        function($scope, issuesService, usersService, projectsService) {
-            var params = {
-                p: 1,
+﻿(function() {
+    function dashboardController($scope, issuesService, usersService) {
+        this.$scope = $scope;
+        this.issuesService = issuesService;
+        this.usersService = usersService;
+        this.initialize();
+        this.refresh();
+    };
+
+    dashboardController.$inject = ['$scope', 'issuesService', 'usersService'];
+
+    dashboardController.prototype = {
+        initialize: function() {
+            this.$scope.users = [];
+            this.$scope.totalIssues = 0;
+            this.$scope.totalDebt = 0;
+            this.$scope.totalDebtStr = '0';
+            this.$scope.issuesProcessed = 0;
+        },
+
+        refresh: function() {
+            this.initialize();
+            this.getTotals();
+            this.getUsers();
+        },
+
+        getCurrentMonth: function() {
+            return new Date(new Date().getFullYear(), new Date().getMonth(), 1).toISOString().substring(0, 10);
+        },
+
+        getSearchParams: function(page) {
+            return {
+                p: page,
                 assigned: true,
-                createdAfter: new Date(new Date().getFullYear(), new Date().getMonth(), 1).toISOString().substring(0, 10),
+                createdAfter: this.getCurrentMonth(),
                 statuses: 'CONFIRMED,OPEN',
                 ps: 5000
             };
+        },
 
-            $scope.totalDebt = 0;
+        getTotals: function(page) {
+            var self = this;
 
-            $scope.searchIssues = function() {
-                var issuesAlreadyRead = 0;
-                var readIssues = function() {
-                    params.p = params.p + 1;
-                    issuesService.issuesSearch(params)
-                        .then(function(result) {
-                            $scope.totalIssues = result.data.total;
-                            issuesAlreadyRead = issuesAlreadyRead + result.data.issues.length;
-                            var debts = _.map(result.data.issues, 'debt');
-                            _.each(debts, function(debt) {
-                                $scope.totalDebt = $scope.totalDebt + juration.parse(debt);
-                            });
-                            $scope.totalDebtStr = juration.stringify($scope.totalDebt, {
-                                format: 'micro'
-                            });
+            var params = this.getSearchParams(page || 1);
 
-                            if (issuesAlreadyRead < $scope.totalIssues) {
-                                readIssues();
-                            }
-                        });
+            this.issuesService.issuesSearch(params)
+                .then(function(result) {
+                    self.totalsSearchCallback(result.data);
+                });
+        },
+
+        totalsSearchCallback: function(data) {
+            this.$scope.issuesProcessed = data.issues.length;
+            this.$scope.totalIssues = data.total;
+
+            if (this.$scope.issuesProcessed <= this.$scope.totalIssues && data.issues.length > 0) {
+                this.calculateTotalDebt(data);
+            }
+        },
+
+        calculateTotalDebt: function(data) {
+            var self = this;
+            var debtArray = _.map(data.issues, 'debt');
+
+            if (!debtArray) {
+                return;
+            }
+
+            angular.forEach(debtArray, function(debt) {
+                if (!debt) {
+                    return;
+                }
+
+                self.$scope.totalDebt += juration.parse(debt);
+            });
+
+            if (self.$scope.totalDebt) {
+                self.$scope.totalDebtStr = juration.stringify(self.$scope.totalDebt, {
+                    format: 'micro'
+                });
+            }
+
+            self.getTotals(data.p + 1);
+        },
+
+        getUsers: function() {
+            var self = this;
+
+            this.usersService.usersSearch(null)
+                .then(function(result) {
+                    self.usersSearchCallback(result.data.users);
+                });
+        },
+
+        usersSearchCallback: function(users) {
+            var self = this;
+
+            this.$scope.users = this.filterUsers(users);
+
+            angular.forEach(this.$scope.users, function(user) {
+                self.getUsersIssuesAndDebt(user);
+            });
+        },
+
+        filterUsers: function(users) {
+            return _.filter(users, function(user) {
+                return user.login !== 'admin';
+            });
+        },
+
+        getUsersIssuesAndDebt: function(user) {
+            var self = this;
+
+            var issuesParam = this.getSearchParams(1);
+            issuesParam.assignees = user.login;
+
+            this.issuesService.issuesSearch(issuesParam)
+                .then(function(result) {
+                    self.userIssuesAndDebtSearchCallback(user, result.data);
+                });
+        },
+
+        userIssuesAndDebtSearchCallback: function(user, data) {
+            user.totalIssues = data.total;
+
+            var groupedIssues = _.groupBy(data.issues, 'rule');
+
+            user.issues = _.map(groupedIssues, function(value, index) {
+                return {
+                    rule: index,
+                    count: value.length,
+                    debt: _.reduce(value, function(memo, num) {
+                        return memo + juration.parse(num.debt);
+                    }, 0)
                 };
+            });
 
-                issuesService.issuesSearch(params)
-                    .then(function(result) {
-                        $scope.totalIssues = result.data.total;
-                        issuesAlreadyRead = result.data.issues.length;
-                        var debts = _.map(result.data.issues, 'debt');
-                        _.each(debts, function(debt) {
-                            $scope.totalDebt = $scope.totalDebt + juration.parse(debt);
-                        });
-                        $scope.totalDebtStr = juration.stringify($scope.totalDebt, {
-                            format: 'micro'
-                        });
-                        readIssues();
-                    });
-            };
+            var debts = _.map(user.issues, 'debt');
 
+            user.totalDebt = _.reduce(debts, function(memo, num) {
+                return memo + num;
+            });
 
-            $scope.searchUsers = function() {
-                usersService.usersSearch(null)
-                    .then(function(result) {
-                        $scope.users = _.filter(result.data.users, function(user) {
-                            return user.login !== 'admin';
-                        });
-
-                        angular.forEach($scope.users, function(user) {
-                            var issuesParam = angular.copy(params);
-                            issuesParam.p = 1;
-                            issuesParam.assignees = user.login;
-                            issuesService.issuesSearch(issuesParam)
-                                .then(function(result) {
-                                    user.totalIssues = result.data.total;
-                                    var groupedIssues = _.groupBy(result.data.issues, 'rule');
-                                    user.issues = _.map(groupedIssues, function(value, index) {
-                                        return {
-                                            rule: index,
-                                            count: value.length,
-                                            debt: _.reduce(value, function(memo, num) {
-                                                return memo + juration.parse(num.debt);
-                                            }, 0)
-                                        };
-                                    });
-                                    var debts = _.map(user.issues, 'debt');
-                                    user.totalDebt = _.reduce(debts, function(memo, num) {
-                                        return memo + num;
-                                    });
-                                    user.totalDebtStr = juration.stringify(user.totalDebt, {
-                                        format: 'micro'
-                                    });
-                                });
-                        });
-                    });
-            };
-
-            $scope.refreshAll = function() {
-                $scope.users = [];
-                $scope.searchIssues();
-                $scope.searchUsers();
-            };
-
-            $scope.projects = [];
-            $scope.teams = [];
-            $scope.teamMembers = [];
-            $scope.getProjects = function() {
-                projectsService.projectsSearch()
-                    .then(function(projects) {
-                        $scope.projects = projects.data.value;
-                        $scope.teams = [];
-                        $scope.teamMembers = [];
-                        angular.forEach($scope.projects, function(project) {
-                            projectsService.teamsSearch(project.id)
-                                .then(function(teams) {
-                                    $scope.teams.push(teams.data.value[0]);
-                                    angular.forEach($scope.teams, function(team) {
-                                        projectsService.membersSearch(project.id, team.id)
-                                            .then(function(teamMembers) {
-                                                $scope.teamMembers = teamMembers.data.value;
-
-                                                $scope.searchIssues();
-                                                $scope.searchUsers();
-                                            });
-                                    });
-                                });
-                        });
-                    })
-            };
-
-            $scope.getProjects();
-        }
-    ])
-    .config([
-        '$routeProvider',
-        function($routeProvider) {
-            $routeProvider.when('/dashboard', {
-                controller: 'dashboardController',
-                templateUrl: 'app/dashboard/index.html'
+            user.totalDebtStr = juration.stringify(user.totalDebt, {
+                format: 'micro'
             });
         }
-    ]);
+    }
+
+    dashboardModule
+        .controller('dashboardController', dashboardController)
+        .config([
+            '$routeProvider',
+            function($routeProvider) {
+                $routeProvider.when('/dashboard', {
+                    controller: 'dashboardController',
+                    templateUrl: 'app/dashboard/index.html'
+                });
+            }
+        ]);
+})();
